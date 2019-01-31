368835e4ed699d033896d9a12fbfec662698be43
Your Name <you@example.com>
2019-01-31 00:16:25 +0000

    clustering  and start cie00 sorting

--- colors_clustering.py
+++ colors_clustering.py
@@ -7,101 +7,67 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
 from constants import *
 from utils import *
+from sklearn.preprocessing import MinMaxScaler
 import matplotlib
 from sklearn.decomposition import PCA
 import math
 import colorsys
 
-#   Cluster colors
+def preprocess_pixel_matrix(pixel_matrix):
 
-def cluster_colors_rgb(pixel_matrix, n_clusters=10, filename='tmp.jpg'):
+    new_matrix = matplotlib.colors.rgb_to_hsv(pixel_matrix/255)
+    return new_matrix
+
+def cluster_colors_rgb(pixel_matrix, n_clusters=10, filename='hello'):
 
     #   Scale RGB colors
-    scale_colors = pixel_matrix / 255
+    pixel_matrix = preprocess_pixel_matrix(pixel_matrix)
 
     #   Create kmeans, fit and rescale colors
     kmeans = KMeans(n_clusters=n_clusters, verbose=3)
-    kmeans.fit(scale_colors)
-    cluster_colors = kmeans.cluster_centers_ * 255
-    cluster_colors = cluster_colors.astype(np.int)
+    kmeans.fit(pixel_matrix)
+
+    cluster_centers = kmeans.cluster_centers_
+    cluster_centers_rgb = matplotlib.colors.hsv_to_rgb(cluster_centers)
+
+    cluster_colors_df = pd.DataFrame()
+    cluster_colors_df['R'] = cluster_centers_rgb[:, 0]*255
+    cluster_colors_df['G'] = cluster_centers_rgb[:, 1]*255
+    cluster_colors_df['B'] = cluster_centers_rgb[:, 2]*255
+
+    cluster_colors_df['R'] = cluster_colors_df['R'].apply(np.int)
+    cluster_colors_df['G'] = cluster_colors_df['G'].apply(np.int)
+    cluster_colors_df['B'] = cluster_colors_df['B'].apply(np.int)
 
     df = pd.DataFrame()
     df['points'] = kmeans.labels_
-
     df = df.groupby(by=['points'])['points'].count().rename().reset_index()
     df.columns = ['points', 'count']
+    draw_colors(df, cluster_centers_rgb, filename)
 
-    #   Scale color size extracting the percentage
-    total_pixels = np.sum(df['count'].values)
-    colors_proportions = df['count'].values / total_pixels * 100 * 100
-    colors_proportions = colors_proportions.astype(np.int)
-    print(colors_proportions)
+    color_scheme = pd.DataFrame(cluster_centers_rgb, columns=['R', 'G', 'B'])
+    color_scheme.to_csv('{}/color_scheme.csv'.format(OUTPUT_FOLDER))
 
-    # Generate colors image
-    image_row = None
-    for idx, c in enumerate(cluster_colors):
-        tmp_arr = np.repeat([c], colors_proportions[idx], axis=0)
-        if image_row is None:
-            image_row = tmp_arr
-        else:
-            image_row = np.concatenate((image_row, tmp_arr), axis=0)
-
-    #   Sort colors by HSV
-    hsv = matplotlib.colors.rgb_to_hsv(image_row)
-    hsv_df = pd.DataFrame(hsv, columns=['h', 's', 'v'])
-    hsv_rounded_df = hsv_df.round(2)
-    hsv_rounded_df.columns = ['h_rounded', 's_rounded', 'v_rounded']
-
-    hsv_df = pd.concat([hsv_df, hsv_rounded_df], axis=1)
-    sorted_hsv = hsv_df.sort_values(['h_rounded', 'v_rounded', 's_rounded'], ascending=[True, True, True])[
-        ['h', 's', 'v']].values
-
-    rgb = matplotlib.colors.hsv_to_rgb(sorted_hsv)
-
-    cv2.imwrite(filename, np.repeat([rgb], 500, axis=0))
-    print('Done')
     return kmeans
+
+
 
 def predict_clusters(pixel_matrix, kmeans, filename):
     #   Scale RGB colors
-    scale_colors = pixel_matrix / 255
-    clusters = kmeans.predict(scale_colors)
+    pixel_matrix = preprocess_pixel_matrix(pixel_matrix)
+    clusters = kmeans.predict(pixel_matrix)
+
+    print(clusters)
+
+    cluster_centers = kmeans.cluster_centers_
+    cluster_centers_rgb = matplotlib.colors.hsv_to_rgb(cluster_centers)
+
     df = pd.DataFrame()
     df['points'] = clusters
-
     df = df.groupby(by=['points'])['points'].count().rename().reset_index()
     df.columns = ['points', 'count']
-
-    #   Scale color size extracting the percentage
-    total_pixels = np.sum(df['count'].values)
-    colors_proportions = df['count'].values / total_pixels * 100 * 100
-    colors_proportions = colors_proportions.astype(np.int)
-    print(colors_proportions)
-
-    colors = (kmeans.cluster_centers_*255).astype(np.int)
-
-    # Generate colors image
-    image_row = None
-    for idx, c in df.iterrows():
-        tmp_arr = np.repeat([colors[df.iloc[idx].points]], colors_proportions[idx], axis=0)
-        if image_row is None:
-            image_row = tmp_arr
-        else:
-            image_row = np.concatenate((image_row, tmp_arr), axis=0)
-
-    #   Sort colors by HSV
-    hsv = matplotlib.colors.rgb_to_hsv(image_row)
-    hsv_df = pd.DataFrame(hsv, columns=['h', 's', 'v'])
-    hsv_rounded_df = hsv_df.round(2)
-    hsv_rounded_df.columns = ['h_rounded', 's_rounded', 'v_rounded']
-
-    hsv_df = pd.concat([hsv_df, hsv_rounded_df], axis=1)
-    sorted_hsv = hsv_df.sort_values(['h_rounded', 'v_rounded', 's_rounded'], ascending=[True, True, True])[
-        ['h', 's', 'v']].values
-
-    rgb = matplotlib.colors.hsv_to_rgb(sorted_hsv)
-
-    cv2.imwrite(filename, np.repeat([rgb], 500, axis=0))
+    draw_colors(df, cluster_centers_rgb, '{}.jpg'.format(filename))
+    df.to_csv('{}_data.csv'.format(filename), index=False)
     print('Done')
 
 
@@ -119,7 +85,7 @@ for idx, row in data.iterrows():
         stacked_images = reshaped
     else:
         stacked_images = np.concatenate((stacked_images, reshaped))
-fitted_model = cluster_colors_rgb(stacked_images, 100, filename='{}/colors_{}.jpg'.format(OUTPUT_FOLDER, 'total'))
+fitted_model = cluster_colors_rgb(stacked_images, 50, filename='{}/colors_{}.jpg'.format(OUTPUT_FOLDER, 'total'))
 
 
 for year in data['year'].unique():
@@ -132,4 +98,4 @@ for year in data['year'].unique():
             stacked_images = reshaped
         else:
             stacked_images = np.concatenate((stacked_images, reshaped))
-    predict_clusters(stacked_images, fitted_model, filename='{}/colors_{}.jpg'.format(OUTPUT_FOLDER, year))
+    predict_clusters(stacked_images, fitted_model, filename='{}/colors_{}'.format(OUTPUT_FOLDER, year))
--- utils.py
+++ utils.py
@@ -1,6 +1,51 @@
 import cv2
+import numpy as np
+import pandas as pd
+import matplotlib
+from colormath.color_objects import LabColor
+from colormath.color_diff import delta_e_cie1976, delta_e_cie2000
 
 
 def load_image(filename):
     img = cv2.imread(filename)
     return img
+
+def draw_colors(df_points, clusters_centers, filename):
+    #   Scale color size extracting the percentage
+    total_pixels = np.sum(df_points['count'].values)
+    colors_proportions = df_points['count'].values / total_pixels * 100 * 100
+    colors_proportions = colors_proportions.astype(np.int)
+    print(colors_proportions)
+
+    colors = (clusters_centers * 255).astype(np.int)
+
+    # Generate colors image
+    image_row = None
+    for idx, c in df_points.iterrows():
+        tmp_arr = np.repeat([colors[df_points.iloc[idx].points]], colors_proportions[idx], axis=0)
+        if image_row is None:
+            image_row = tmp_arr
+        else:
+            image_row = np.concatenate((image_row, tmp_arr), axis=0)
+
+    #   Sort colors by HSV
+    hsv = matplotlib.colors.rgb_to_hsv(image_row)
+    hsv_df = pd.DataFrame(hsv, columns=['h', 's', 'v'])
+    hsv_rounded_df = hsv_df.round(2)
+    hsv_rounded_df.columns = ['h_rounded', 's_rounded', 'v_rounded']
+
+    hsv_df = pd.concat([hsv_df, hsv_rounded_df], axis=1)
+    sorted_hsv = hsv_df.sort_values(['h_rounded', 'v_rounded', 's_rounded'], ascending=[True, True, True])[
+        ['h', 's', 'v']].values
+
+    '''
+    # Reference color.
+    color1 = LabColor(lab_l=0.9, lab_a=16.3, lab_b=-2.22)
+    # Color to be compared to the reference.
+    color2 = LabColor(lab_l=0.7, lab_a=14.2, lab_b=-1.80)
+    # This is your delta E value as a float.
+    delta_e = delta_e_cie1976(color1, color2)
+    '''
+    rgb = matplotlib.colors.hsv_to_rgb(sorted_hsv)
+
+    cv2.imwrite(filename, np.repeat([rgb], 500, axis=0))
